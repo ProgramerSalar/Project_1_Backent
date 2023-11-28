e103f43bf8af6a3317f76e6f46bc7b4962443620
manish <manishkumar60708090@gmail.com>
2023-11-28 19:46:03 +0530

    Configure server side upload

--- routes/order.js
+++ routes/order.js
@@ -4,21 +4,6 @@ const { Order } = require("../models/order.js");
 const { OrderItem } = require("../models/order-item.js");
 
 
-
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, '/tmp/my-uploads')
-    },
-    filename: function (req, file, cb) {
-      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
-      cb(null, file.fieldname + '-' + uniqueSuffix)
-    }
-  })
-  
-  const upload = multer({ storage: storage })
-
-
-
   
 
 router.get(`/`, async (req, res) => {
--- routes/products.js
+++ routes/products.js
@@ -4,35 +4,40 @@ const {Product} = require('../models/product.js')
 const express = require('express')
 const router = express.Router()
 const mongoose = require('mongoose')
-
-router.get(`/`, async (req, res) =>{
-    // localhost:3000/api/v1/products?categories=2342342,234234
-    let filter = {};
-    if(req.query.categories)
-    {
-         filter = {category: req.query.categories.split(',')}
-    }
-
-    const productList = await Product.find(filter).populate('category');
-
-    if(!productList) {
-        res.status(500).json({success: false})
-    } 
-    res.send(productList);
-})
+const multer = require("multer")
 
 
-router.post('/', async(req, res) => {
+
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'public/uploads')
+    },
+    filename: function (req, file, cb) {
+        const fileName = file.originalname.split(' ').join('-');
+        cb(null, fileName, + '-', Date.now());
+    },
+});
+
+const uploadOptions = multer({ storage: storage });
+
+
+
+
+router.post('/', uploadOptions.single('image'), async(req, res) => {
     
 
     const category = await Category.findById(req.body.category);
     if(!category) return res.status(400).send('Invalid Category')
+    const fileName = req.file.filename
+    // const basePath = `${req.protocol}://${req.get('host')}/public/upload/`
+    const basePath = `${req.protocol}://${req.get('host')}/public/uploads/`;
 
     let product = new Product({
         name:req.body.name,
         description:req.body.description,
         richDescription:req.body.richDescription,
-        image:req.body.image,
+        image:`${basePath}${fileName}`,
         brand:req.body.brand,
         price:req.body.price,
         category:req.body.category,
@@ -49,6 +54,23 @@ router.post('/', async(req, res) => {
     res.send(product)
 
     
+})
+
+
+router.get(`/`, async (req, res) =>{
+    // localhost:3000/api/v1/products?categories=2342342,234234
+    let filter = {};
+    if(req.query.categories)
+    {
+         filter = {category: req.query.categories.split(',')}
+    }
+
+    const productList = await Product.find(filter).populate('category');
+
+    if(!productList) {
+        res.status(500).json({success: false})
+    } 
+    res.send(productList);
 })
 
 
